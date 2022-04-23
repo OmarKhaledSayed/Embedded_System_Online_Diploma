8630d31439be3abdabb5551c5578c726ced43e4e
OmarKhaledSayed <omar.khaled0107@gmail.com>
2022-04-23 20:00:13 +0200

    Unit 2 C programming

--- Unit_2_C_Programming/Lecture_3_C_Basics/Assignments/Assignment1_Ex7/Assignment1_Ex7.c
+++ Unit_2_C_Programming/Lecture_3_C_Basics/Assignments/Assignment1_Ex7/Assignment1_Ex7.c
@@ -1,23 +1,24 @@
 /*
- * Assignment1_Ex6.c
+ * Assignment1_Ex7.c
  *
  *  Created on: Apr 23, 2022
  *      Author: Omar Khaled
- *  Write Source Code to Swap Two Numbers
+ *
+	C program to Swap Two Numbers without temporary variable
  */
 
 #include <stdio.h>
 int main(){
 	setvbuf(stdout, NULL,_IONBF, 0);
 	setvbuf(stderr, NULL,_IONBF, 0);
-	float a, b, c;
+	float a, b;
 	printf("Enter value of a: ");
 	scanf("%f", &a);
 	printf("Enter value of b: ");
 	scanf("%f", &b);
-	c = a;
-	a = b;
-	b = c;
+	a = a + b;
+	b = a - b;
+	a = a - b;
 	printf("After swapping, value of a = %f\n", a);
 	printf("After swapping, value of b = %f\n", b);
 	return 0;
